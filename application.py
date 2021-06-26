5abbcd947daafeb81a51ccd91bb295438638fb2c
Conor Walsh <cewalsh@bu.edu>
2021-06-26 00:50:34 -0400

    created template page where you can upload an image

--- application.py
+++ application.py
@@ -1,10 +1,20 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = "./upload/"
 
 
 
 @app.route('/')
 def test():
     return "Hello"
+
+@app.route('/upload-image', methods=['Get', 'POST'])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            return render_template("upload.html", uploaded_image=image.filename)
+    return render_template("upload.html")
