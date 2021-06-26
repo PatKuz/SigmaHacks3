aa17dd351a7810288db3f03ca123915dab3e0b62
PatKuz <>
2021-06-26 07:56:58 -0400

    UI

--- application.py
+++ application.py
@@ -2,16 +2,12 @@ from flask import Flask, request, render_template
 import os
 from evaluation import get_prediction
 
-
 app = Flask(__name__)
 app.config["IMAGE_UPLOADS"] = "./upload/"
 
-
-
 @app.route('/')
 def test():
-    return render_template("index.html")
-
+    return render_template("upload.html")
 
 @app.route('/information', methods=['GET'])
 def info():
@@ -20,20 +16,12 @@ def info():
 @app.route('/upload', methods=['Get', 'POST'])
 def upload_image():
     if request.method == "POST":
-        print("This was called")
         if request.files:
-            print("Got the file")
             image = request.files["image"]
             image.save(os.path.join(app.config["IMAGE_UPLOADS"], 'file.jpg'))
-            #will call evulation
             response = get_prediction('upload/file.jpg')
-            print("Printing payload")
             x = response.payload
             for item in response.payload:
                 result = item.display_name
-            # print(result.payload)
-            #get result
-            #delete image
-            #return result
             return render_template("upload.html", result=result)
     return render_template("upload.html")
